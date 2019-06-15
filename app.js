fc6e4431855320155751945de69274040dc70dd0
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-06-16 01:15:54 +1000

    initial commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// connect to mongoose
+mongoose
+  .connect("mongodb://localhost/vidjot-dev", { useNewUrlParser: true })
+  .then(() => {
+    console.log("MongoDB Connected")
+  })
+  .catch(err => {
+    console.log(`Error: ${err}`)
+  });
+
+// Load Idea model
+require('./models/Idea')
+const Idea = mongoose.model('ideas');
+
+// handlebars middleware
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+  }));
+app.set("view engine", "handlebars");
+
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+// Index Route
+app.get("/", (req, res) => {
+  const title = "Welcome";
+  res.render("index", {
+    title: title
+  });
+});
+
+// About Route
+app.get("/about", (req, res) => {
+  res.render("about")
+});
+
+// Add Idea Form
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add');
+});
+
+// Process form
+app.post('/ideas', (req, res) => {
+  console.log(req.body);
+  res.send('ok');
+})
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
+});
