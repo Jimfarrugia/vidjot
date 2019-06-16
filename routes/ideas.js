7ea6960bfafcf3cd74ed058c6dfccbf0df56b146
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-06-17 06:36:21 +1000

    users are now redirected when trying to edit another user's idea (by entering the url)

--- routes/ideas.js
+++ routes/ideas.js
@@ -27,9 +27,14 @@ router.get('/add', ensureAuthenticated, (req, res) => {
 router.get('/edit/:id', ensureAuthenticated, (req, res) => {
   Idea.findOne({ _id: req.params.id })
   .then(idea => {
-    res.render('ideas/edit', {
-      idea: idea
-    });
+    if (idea.user !== req.user.id) {
+      req.flash('error_msg', "Not Authorized!");
+      res.redirect('/ideas');
+    } else {
+      res.render('ideas/edit', {
+        idea: idea
+      });
+    }
   })
 });
 
